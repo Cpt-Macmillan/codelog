f45edf67a9352d5445140b26fb83e883b657dfda
Stewart_feng <stewart_feng@outlook.com>
2020-08-12 16:21:33 +0800

    for file transfer
    8/12

---
+++ rapidJsonLearning.cpp
@@ -0,0 +1,104 @@
+﻿#include"rapidJsonLearning.h"
+#include<iostream>
+#include<string>
+#include<fstream>
+
+#include<vector>
+
+using std::string;
+using std::ifstream;
+using std::vector;
+using std::ios;
+int main() {
+	jsonParser jsonParser1("message.json");
+
+	jsonParser1.DOMparse();
+	return 0;
+	
+
+}
+
+jsonParser::jsonParser()
+{
+	// Do Nothing;
+}
+
+jsonParser::jsonParser(const string& fileName)
+{
+	readFile(fileName);
+}
+
+void jsonParser::readFile(const string& fileName) {
+	ifstream t(fileName);
+	if (!t) {
+		cout << "No file found";
+		return;
+	}
+	IStreamWrapper isw(t);
+	document.ParseStream(isw);
+	return;
+}
+
+void jsonParser::DOMparse()
+{
+	for (Value::ConstValueIterator itr = document.Begin(); itr != document.End(); itr++) {
+		LdapEntry_ temp_LdapEntry;
+		//以下为填入temp_LdapEntry的过程
+		//	以下为填入dn的过程
+		dn_ temp_dn;
+		if (itr->HasMember("dn")) {
+			temp_dn = (*itr)["dn"].GetString();
+		}
+		temp_LdapEntry.dn = temp_dn;
+		//	以上为填入dn的过程
+		//	以下为填入ldapAttributes的过程
+		ldapAttributes_ temp_ldapAttributes;
+		//		以下为填入temp_ldapAttributes的过程
+		if (itr->HasMember("ldapAttributes")) {
+			for (auto ite1 = (*itr)["ldapAttributes"].Begin(); ite1 != (*itr)["ldapAttributes"].End(); ite1++) {
+				LdapAttribute_ temp_ldapAttribute;
+		//			以下为填入temp_ldapAttribute的过程
+
+				name_ temp_name;
+		//				以下为填入temp_name的过程
+				if (ite1->HasMember("name")) {
+					temp_name = (*ite1)["name"].GetString();
+				}
+		//				以上为填入temp_name的过程
+				temp_ldapAttribute.name = temp_name;
+
+				values_ temp_values;
+		//				以下为填入temp_values的过程
+				if (ite1->HasMember("values")) {
+					for (auto ite2 = (*ite1)["values"].Begin(); ite2 != (*ite1)["values"].End(); ite2++) {
+						LdapAttributeValue_ temp_ldapAttributeValue;
+		//					以下为填入temp_ldapAttributeValue的过程
+						temp_ldapAttributeValue = ite2->GetString();
+		//					以上为填入temp_ldapAttributeValue的过程
+						temp_values.push_back(temp_ldapAttributeValue);
+					}
+				}
+		//				以上为填入temp_values的过程
+				temp_ldapAttribute.values = temp_values;
+
+		//			以下为填入temp_ldapAttribute的过程		
+				if(ite1->HasMember("name"))
+		//				以下为填入ldapAttributeName的过程
+		//			以上为填入temp_ldapAttribute的过程
+				temp_ldapAttributes.push_back(temp_ldapAttribute);
+			}
+		}
+		//		以上为填入temp_ldapAttributes的过程
+		temp_LdapEntry.ldapAttributes = temp_ldapAttributes;
+		//	以上为填入ldapAttributes的过程
+
+		
+
+		//以上为填入temp_LdapEntry的过程
+		content.ldapEntries.push_back(temp_LdapEntry);
+	}
+}
+
+
+
+
---
+++ rapidJsonLearning.h
@@ -0,0 +1,94 @@
+﻿// rapidJsonLearning.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<map>
+#include<list>
+#include"rapidjson/document.h"
+#include"rapidjson/istreamwrapper.h"
+#include"rapidjson/pointer.h"
+
+using std::cout;
+using std::string;
+using namespace rapidjson;
+
+/* Related Data Structure */
+//typedef std::string dn_;
+//typedef std::string LdapAttributeValue_;
+//
+//typedef std::vector<LdapAttributeValue_> values_;
+//typedef std::string name_;
+//
+//
+//class LdapAttribute_ {
+//public:
+//	name_ name;
+//	values_ values;
+//};
+//
+//typedef std::vector<LdapAttribute_> ldapAttributes_;
+//class LdapEntry_ {
+//public:
+//	dn_ dn;
+//	ldapAttributes_ ldapAttributes;
+//};
+//
+//
+//typedef std::vector<LdapEntry_> LdapEntries_;
+//
+//
+//
+//struct content_ {
+//	LdapEntries_ LdapEntries;
+//};
+
+  
+/* Generated classes*/
+typedef std::string LdapAttributeValue_;
+typedef std::vector<LdapAttributeValue_> values_;
+typedef std::string name_;
+class LdapAttribute_ {
+public:
+	name_ name;
+	values_ values;
+};
+typedef std::vector<LdapAttribute_> ldapAttributes_;
+typedef std::string dn_;
+class LdapEntry_ {
+public:
+		dn_ dn;
+		ldapAttributes_ ldapAttributes;
+};
+typedef std::vector<LdapEntry_> LdapEntries_;
+class content_ { public: LdapEntries_ ldapEntries;  } content;
+
+
+/* general class for parser */
+
+class jsonParser {  
+public:
+	jsonParser();
+	jsonParser(const string&);
+	void readFile(const string&);
+	void DOMparse();
+private:
+	string jsonFile;
+	Document document;
+	content_ content;
+};
+
+
+
+
+
+
+
+ 
+
+
+
+// TODO: 在此处引用程序需要的其他标头。
